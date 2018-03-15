82f1ed3a691a335e1ef40bf30221eb8321d1c719
Hans Schouten <J.Schouten-1@student.tudelft.nl>
2018-03-15 14:29:19 +0100

    Expanded the NetFlow parsing capabilities

--- src/main/java/org/tudelft/flink/streaming/NetFlow.java
+++ src/main/java/org/tudelft/flink/streaming/NetFlow.java
@@ -1,6 +1,7 @@
 package org.tudelft.flink.streaming;
 
 import java.io.Serializable;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class NetFlow implements Serializable {
@@ -8,30 +9,75 @@ public class NetFlow implements Serializable {
     private static final long serialVersionUID = 8078274017619893051L;
 
     public String json;
+
+    public long flow_size;
+    public long packet_count;
     public String srcIP;
     public String dstIP;
     public String srcPort;
     public String dstPort;
+    public long start;
+    public long end;
 
-    public NetFlow(){}
-
-    public void setFromString(String line) {
+    /**
+     * Set all instance variables to the values encoded in the given JSON string.
+     * @param data
+     */
+    public void setFromString(String data) {
         JsonNode jsonNode = null;
         // parse JSON
-        if (line != null) {
-            jsonNode = JSONUtils.convertToJSON(line.getBytes());
+        if (data != null) {
+            jsonNode = JSONUtils.convertToJSON(data.getBytes());
         }
         // take NetFLow variables from JSON object
         if (jsonNode != null) {
             this.json = jsonNode.toString();
-            JsonNode data = jsonNode.get("DataSets").get(0);
-            this.srcIP = data.get(0).get("V").asText();
-            this.dstIP = data.get(1).get("V").asText();
-            this.srcPort = "80";
-            this.dstPort = "80";
+            JsonNode parameters = jsonNode.get("DataSets").get(0);
+
+            // Store all IPFIX Information Elements in this NetFlow object
+            for (JsonNode parameter : parameters) {
+                Integer id = Integer.parseInt(parameter.get("I").asText());
+                String value = parameter.get("V").asText();
+                storeElement(id, value);
+            }
         }
     }
 
+    /**
+     * Set the instance variable corresponding to the given IPFIX Element ID.
+     * see: https://www.iana.org/assignments/ipfix/ipfix.xhtml
+     *
+     * @param id        the Element ID
+     * @param value     the value
+     */
+    protected void storeElement(Integer id, String value) {
+        switch (id) {
+            case 1:
+                this.flow_size = Long.decode(value);
+                break;
+            case 2:
+                this.packet_count = Long.decode(value);
+                break;
+            case 7:
+                this.srcPort = value;
+                break;
+            case 8:
+                this.srcIP = value;
+                break;
+            case 11:
+                this.dstPort = value;
+                break;
+            case 12:
+                this.dstIP = value;
+                break;
+            case 21:
+                this.start = Long.valueOf(value);
+                break;
+            case 22:
+                this.end = Long.valueOf(value);
+                break;
+        }
+    }
 
     @Override
     public int hashCode() {
